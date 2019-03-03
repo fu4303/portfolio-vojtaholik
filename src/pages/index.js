c66d4e7a8fdca5798f5a492a3259388bfca05e69
Vojta Holik <vojta@egghead.io>
2019-03-03 10:32:05 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ import {
 
 import gif6d from '../../content/drawer/gifs/6DIntro.gif'
 
-// ReactGA.pageview(window.location.pathname + window.location.search)
 ReactGA.initialize('UA-135029522-1')
+ReactGA.pageview(window.location.pathname)
 
 export default function Index({ data: { images, site } }) {
   return (
