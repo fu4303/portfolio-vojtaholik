f6403dc04389a9293f099a99a7e99e0a421d37b8
Vojta Holik <vojta@egghead.io>
2019-03-14 18:30:31 +0100

    update

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,29 +14,13 @@ export default class Slider extends React.Component {
   }
 
   render() {
-    const { children, slides = [], slide1, slide2, slide3, slide4 } = this.props
-    const newArr = Array.from({ children })
-    const child = React.Children.toArray(this.props.children)
+    const { children = [] } = this.props
     return (
       <div>
-        {console.log(child)}
         <Carousel
           value={this.state.value}
           onChange={this.onChange}
-          //   slides={newArr.map(item => (
-          //     <div>{item}</div>
-          //   ))}
-          // slides={[
-          //   <div>{slide1}</div>,
-          //   <div>{slide2}</div>,
-          //   <div>{slide3}</div>,
-          //   <div>{slide4}</div>,
-          // ]}
-
-          slides={slides}
-          //slides={[<div>{children}</div>, 'meow2']}
-          //slides={[<div>{slide1}</div>, <div>{slide2}</div>, <div>three</div>]}
-          //slides={['hey', 'two', 'three']}
+          slides={children}
           arrows
           clickToChange
         />
