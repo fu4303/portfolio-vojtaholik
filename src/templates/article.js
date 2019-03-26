bda0e167fbe4504d39960c4def6768a5aad38a45
Vojta Holik <vojta@egghead.io>
2019-03-26 18:07:03 +0100

    add muzika poster

--- src/templates/article.js
+++ src/templates/article.js
@@ -20,7 +20,7 @@ class ArticleTemplate extends React.Component {
             opacity: 0.7;
             margin-top: 20px;
           `}>
-          <Link to='/'>HOME</Link> /
+          <Link to="/">HOME</Link> /
           <div
             css={css`
               position: absolute;
@@ -28,8 +28,8 @@ class ArticleTemplate extends React.Component {
               padding: 5px 10px;
               margin-left: 50px;
             `}>
-            <Link to='/projects'>Projects</Link> -{' '}
-            <Link to='/screenshots'>Screenshots</Link> -{' '}
+            <Link to="/projects">Projects</Link> -{' '}
+            <Link to="/screenshots">Screenshots</Link> -{' '}
           </div>
           <Link
             to={`/${article.fields.collection}`}
