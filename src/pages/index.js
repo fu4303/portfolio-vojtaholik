70b5903949f90bf6ed1bf7398f752c263d031a74
Vojta Holik <vojta@egghead.io>
2019-07-18 17:23:12 +0200

    uses react-spring and makes a mess :|

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,135 +1,145 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {graphql} from 'gatsby'
-import styled from '@emotion/styled'
-import Link from '../components/link'
-import Img from 'gatsby-image'
-import {css} from '@emotion/core'
-import MDXRenderer from 'gatsby-mdx/mdx-renderer'
-import Masonry from 'react-masonry-component'
-import SEO from '../components/seo'
+import {useTransition, a} from 'react-spring'
+import shuffle from 'lodash/shuffle'
+import useMeasure from '../utils/useMeasure'
+import useMedia from '../utils/useMedia'
 import ReactGA from 'react-ga'
-import {colors, spacing, breakpoints, fonts} from '../utils/styles'
-
-import gif6d from '../../content/drawer/gifs/6DIntro.gif'
-
-ReactGA.initialize('UA-135029522-1')
-
-const masonryOptions = {
-  transitionDuration: 0,
-}
+//import data from './data'
+import './styles.css'
+import Img from 'gatsby-image'
 
 export default function Index({data: {images, site}}) {
+  // Tie media queries to the number of columns
+  const columns = useMedia(
+    ['(min-width: 1500px)', '(min-width: 1000px)', '(min-width: 600px)'],
+    [4, 3, 3],
+    2
+  )
+  // Measure the width of the container element
+  const [bind, {width}] = useMeasure()
+  // Hold items and shuffle them every 2 seconds
+  const [items, set] = useState(images.edges)
+
+  console.log(images.edges)
+
+  //useEffect(() => void setInterval(() => set(shuffle), 2000), [])
+  // Form a grid of stacked items
+  let heights = new Array(columns).fill(0) // Each column gets a height starting with zero
+  let gridItems = items.map((child, i) => {
+    const column = heights.indexOf(Math.min(...heights)) // Basic masonry-grid placing, puts tile into the smallest column using Math.min
+    const xy = [
+      (width / columns) * column,
+      (heights[column] +=
+        child.node.childImageSharp.fluid.presentationHeight / 2) -
+        child.node.childImageSharp.fluid.presentationHeight / 2,
+    ] // X = container width / number of columns * column index, Y = it's just the height of the current column
+    return {
+      ...child,
+      xy,
+      width: width / columns,
+      height: child.node.childImageSharp.fluid.presentationHeight / 2,
+    }
+  })
+  // Turn the static grid values into animated transitions, any addition, removal or change will be animated
+  const transitions = useTransition(
+    gridItems,
+    item => item.node.childImageSharp.fluid.src,
+    {
+      from: ({xy, width, height}) => ({xy, width, height, opacity: 0}),
+      enter: ({xy, width, height}) => ({xy, width, height, opacity: 1}),
+      update: ({xy, width, height}) => ({xy, width, height}),
+      leave: {height: 0, opacity: 0},
+      config: {mass: 5, tension: 500, friction: 100},
+      trail: 25,
+    }
+  )
+  // Render the grid
   return (
-    <>
-      <SEO title={site.siteMetadata.title} />
-      <Main>
-        <div
-          css={css`
-            .image-name {
-              position: absolute;
-              margin-left: 0px;
-              a {
-                color: hsla(231, 76%, 56%, 0.6);
+    <div {...bind} className="list" style={{height: Math.max(...heights)}}>
+      {transitions.map(({item, props: {xy, ...rest}, key}) => (
+        <a.div
+          key={item.node.id}
+          style={{
+            transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
+            ...rest,
+            a: {
+              color: 'hsla(231, 76%, 56%, 0.6)',
+            },
+            fontSize: '13px',
+            lineHeight: '1.2',
+            color: 'hsla(0, 0%, 0%, 0.7)',
+          }}>
+          <div
+            style={{
+              backgroundImage: `url(${item.node.childImageSharp.fluid.src})`,
+              marginBottom: 5,
+            }}>
+            {' '}
+            <a
+              style={{
+                width: '100%',
+                height: '100%',
+                display: 'block',
+                cursor: 'zoom-in',
+              }}
+              href={item.node.childImageSharp.original.src}
+              target="_blank"
+              onClick={() =>
+                ReactGA.event({
+                  action: `clicked ${item.node.name.substring(4)}`,
+                  category: 'drawer images',
+                })
               }
-              @media (min-width: ${breakpoints.tablet}px) {
-                margin-top: 8px;
-                font-size: 13px;
-              }
+              rel="noopener noreferrer"
+            />
+          </div>
 
-              margin-top: 5px;
-              font-size: 12px;
-              line-height: 1.2;
-              color: hsla(0, 0%, 0%, 0.5);
-            }
-            .gatsby-image-wrapper {
-              box-shadow: 0 30px 80px -20px hsla(0, 0%, 0%, 0.2);
-              cursor: zoom-in;
-            }
-            .grid-item {
-              width: 100%;
-              @media (min-width: ${breakpoints.tablet}px) {
-                max-width: 33.33333%;
-                padding: 25px;
-              }
-
-              max-width: 50%;
-              padding: 20px 8px;
-            }
-            .grid-item-inner {
-              :hover {
-                .image-name {
-                  a {
-                    color: hsla(231, 76%, 56%, 0.9);
-                  }
-                  color: hsla(0, 0%, 0%, 0.8);
-                }
-              }
-            }
-          `}>
-          <Masonry className={'masonry-item'}>
-            {images.edges.map(({node: data}) => (
-              <div className="grid-item" key={data.id}>
-                <div className="grid-item-inner">
-                  <a
-                    href={data.childImageSharp.original.src}
-                    target="_blank"
-                    onClick={() =>
-                      ReactGA.event({
-                        action: `clicked ${data.name.substring(4)}`,
-                        category: 'drawer images',
-                      })
-                    }
-                    rel="noopener noreferrer">
-                    <Img
-                      alt={`${data.name}`}
-                      sizes={data.childImageSharp.fluid}
-                    />
-                  </a>
-                  {data.name.match('-') ? (
-                    <span className="image-name">
-                      {data.name
-                        .substring(4)
-                        .replace('@2x', '')
-                        .split('-')
-                        .map((name, dash) => (
-                          <span key={name}>
-                            {name}
-                            {dash >= 0 && ' '}
-                            {/* Curation is key */}
-                            {name === 'for joel' && (
-                              <span>
-                                | <a href="https://joelhooks.com">visit</a>
-                              </span>
-                            )}{' '}
-                            {name === 'illustrated.dev' && (
-                              <span>
-                                | <a href="https://illustrated.dev">visit</a>
-                              </span>
-                            )}{' '}
-                            {name === 'howtoegghead.com' && (
-                              <span>
-                                | <a href="https://howtoegghead.com">visit</a>
-                              </span>
-                            )}{' '}
-                            {name === 'kentcdodds.com' && (
-                              <span>
-                                | <a href="https://kentcdodds.com">visit</a>
-                              </span>
-                            )}
-                            {name === 'moonhighway.com' && (
-                              <span>
-                                | <a href="https://moonhighway.com">visit</a>
-                              </span>
-                            )}
-                            {name === 'gatsby starter egghead blog' && (
-                              <span>
-                                |{' '}
-                                <a href="https://github.com/eggheadio/gatsby-starter-egghead-blog">
-                                  repo
-                                </a>
-                              </span>
-                            )}
-                            {/* {name === 'disruptive' && (
+          {item.node.name.match('-') ? (
+            <span>
+              {item.node.name
+                .substring(4)
+                .replace('@2x', '')
+                .split('-')
+                .map((name, dash) => (
+                  <span key={name}>
+                    {name}
+                    {dash >= 0 && ' '}
+                    {/* Curation is key */}
+                    {name === 'for joel' && (
+                      <>
+                        | <a href="https://joelhooks.com">visit</a>
+                      </>
+                    )}{' '}
+                    {name === 'illustrated.dev' && (
+                      <>
+                        | <a href="https://illustrated.dev">visit</a>
+                      </>
+                    )}{' '}
+                    {name === 'howtoegghead.com' && (
+                      <>
+                        | <a href="https://howtoegghead.com">visit</a>
+                      </>
+                    )}{' '}
+                    {name === 'kentcdodds.com' && (
+                      <>
+                        | <a href="https://kentcdodds.com">visit</a>
+                      </>
+                    )}
+                    {name === 'moonhighway.com' && (
+                      <>
+                        | <a href="https://moonhighway.com">visit</a>
+                      </>
+                    )}
+                    {name === 'gatsby starter egghead blog' && (
+                      <>
+                        |{' '}
+                        <a href="https://github.com/eggheadio/gatsby-starter-egghead-blog">
+                          repo
+                        </a>
+                      </>
+                    )}
+                    {/* {name === 'disruptive' && (
                               <span>
                                 |{' '}
                                 <a href="https://6dacademy.com/disruptive/">
@@ -137,108 +147,40 @@ export default function Index({data: {images, site}}) {
                                 </a>
                               </span>
                             )} */}
-                            {name === 'track' && (
-                              <span>
-                                |{' '}
-                                <a href="https://soundcloud.com/komox/kmx-day-of-the-riff">
-                                  listen
-                                </a>
-                              </span>
-                            )}
-                          </span>
-                        ))}
-                    </span>
-                  ) : (
-                    <span className="image-name">{data.name}</span>
-                  )}
-                </div>
-              </div>
-            ))}
-            <div className="grid-item image-name">
-              <a href="https://www.6dacademy.com/">
-                <img alt="6d gif" src={gif6d} css={css({marginBottom: 8})} />
-              </a>
-              <span>
-                <a href="https://www.6dacademy.com/">visit 6dacademy.com</a>
-              </span>
-            </div>
-          </Masonry>
-          {/* <div>
-          {text.edges.map(({ node: data }) => (
-            <div
-              key={data.id}
-              className='grid-item'
-              css={css`
-                width: 100%;
-
-                background: yellow;
-                padding: 40px;
-                background: #fafafa;
-              `}>
-              <Link to={`/${data.fields.slug}`}>{data.frontmatter.title}</Link>
-            </div>
-          ))}
-        </div> */}
-          <br />
-        </div>
-        {/* <em>Man we would be all totally lost without screenshots.</em>
-        Vojta
-        <br />
-        <p>it's all about showing process.</p> */}
-      </Main>
-    </>
+                    {name === 'track' && (
+                      <span>
+                        |{' '}
+                        <a href="https://soundcloud.com/komox/kmx-day-of-the-riff">
+                          listen
+                        </a>
+                      </span>
+                    )}
+                  </span>
+                ))}
+            </span>
+          ) : (
+            <span className="image-name">{item.node.name}</span>
+          )}
+          {/* yo <img src={item.node.childImageSharp.original.src} /> */}
+        </a.div>
+        // <a.div
+        //   key={key}
+        //   style={{
+        //     transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
+        //     ...rest,
+        //   }}>
+        //   {/* <div style={{backgroundImage: item.css}} /> */}
+        // </a.div>
+      ))}
+    </div>
   )
 }
 
-const Main = styled.main`
-  margin: 0 auto;
-  background: #f1f1f1;
-  @media (min-width: ${breakpoints.desktop}px) {
-    padding: 20px;
-  }
-  padding: 5px;
-  h2 {
-    margin-top: 2em;
-  }
-  h3 {
-    font-size: 24px;
-    line-height: 1;
-  }
-`
-
-export const pageQuery = graphql`
-  query IndexPageQuery {
+export const newPageQuery = graphql`
+  query NewIndexPageQuery {
     site {
       siteMetadata {
         title
-      }
-    }
-    text: allMdx(filter: {fields: {collection: {eq: "drawer"}}}) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-            collection
-          }
-          frontmatter {
-            title
-          }
-        }
-      }
-    }
-    gifs: allFile(
-      filter: {extension: {regex: "/gif/"}, sourceInstanceName: {eq: "drawer"}}
-      sort: {order: DESC, fields: relativePath}
-    ) {
-      edges {
-        node {
-          name
-          absolutePath
-          relativePath
-          relativeDirectory
-          sourceInstanceName
-        }
       }
     }
     images: allFile(
@@ -258,8 +200,11 @@ export const pageQuery = graphql`
           childImageSharp {
             original {
               src
+              height
             }
-            fluid(maxWidth: 400) {
+            fluid(maxWidth: 900) {
+              src
+              presentationHeight
               ...GatsbyImageSharpFluid_tracedSVG
             }
           }
