57bb7b12294338cb5572588a9a7d2955edb6411a
Vojta Holik <vojta@egghead.io>
2020-02-07 15:50:08 +0100

    height auto

--- content/posts/lottie-animations-in-react-and-mdx/animationRenderer.js
+++ content/posts/lottie-animations-in-react-and-mdx/animationRenderer.js
@@ -2,8 +2,7 @@ import React from "react"
 import Lottie from "react-lottie"
 import { IconButton } from "@theme-ui/components"
 import { Icon } from "react-icons-kit"
-import { play2, pause, spinner8 } from "react-icons-kit/icomoon"
-import { motion } from "framer-motion"
+import { play2, pause } from "react-icons-kit/icomoon"
 
 export default function AnimationRenderer(props) {
   const [isPaused, setPaused] = React.useState(false)
@@ -26,7 +25,6 @@ export default function AnimationRenderer(props) {
         isPaused={isPaused}
         isClickToPauseDisabled={true}
       />
-
       <IconButton opacity={0.6} onClick={() => setPaused(!isPaused)}>
         {isPaused ? (
           <Icon size={24} icon={play2} />
