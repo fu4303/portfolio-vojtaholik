1754e7ea2977531824e76033433ea26da74ded0d
Vojta Holik <vojta@egghead.io>
2020-02-07 14:49:36 +0100

    fix

--- content/posts/lottie-animations-in-react-and-mdx/animationRenderer.js
+++ content/posts/lottie-animations-in-react-and-mdx/animationRenderer.js
@@ -4,11 +4,9 @@ import { IconButton } from "@theme-ui/components"
 import { Icon } from "react-icons-kit"
 import { play2, pause, spinner8 } from "react-icons-kit/icomoon"
 import { motion } from "framer-motion"
-import fetch from "isomorphic-unfetch"
 
 export default function AnimationRenderer(props) {
   const [isPaused, setPaused] = React.useState(false)
-  const [isLoaded, setLoaded] = React.useState(true)
 
   const defaultOptions = {
     loop: props.loop || true,
@@ -18,17 +16,10 @@ export default function AnimationRenderer(props) {
       preserveAspectRatio: "xMidYMid slice",
     },
   }
-  const eventListeners = [
-    {
-      eventName: "data_ready",
-      callback: () => setLoaded(true),
-    },
-  ]
 
-  return props.animation && isLoaded ? (
+  return props.animation ? (
     <div style={{ display: "flex", alignItems: "flex-end", marginBottom: 40 }}>
       <Lottie
-        eventListeners={eventListeners}
         options={defaultOptions}
         width={props.width || "100%"}
         height={props.height || "100%"}
@@ -44,35 +35,37 @@ export default function AnimationRenderer(props) {
         )}
       </IconButton>
     </div>
-  ) : isLoaded ? (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: 480,
-      }}
-    >
-      Missing animation data
-    </div>
   ) : (
     <div
       style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        height: 480,
+        height: props.height || "100%",
       }}
     >
-      <motion.div
-        initial={{ rotate: 0 }}
-        animate={{ rotate: [0, 360] }}
-        transition={{
-          loop: Infinity,
-        }}
-      >
-        <Icon size={32} icon={spinner8} />
-      </motion.div>
+      Missing animation data
     </div>
   )
+}
+
+{
+  /* <div
+style={{
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: props.height || "100%",
+}}
+>
+<motion.div
+  initial={{ rotate: 0 }}
+  animate={{ rotate: [0, 360] }}
+  transition={{
+    loop: Infinity,
+  }}
+>
+  <Icon size={32} icon={spinner8} />
+</motion.div>
+</div> */
 }
