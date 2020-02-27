ffd5199bd3c915bf7e76761347d14c91edf206fa
Vojta Holik <vojta@egghead.io>
2020-02-27 14:23:57 +0100

    add sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-manifest`,
