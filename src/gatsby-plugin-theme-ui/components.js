ec7d6c9f8809fe5f07883d38963d3437cde07bd0
Vojta Holik <vojta@egghead.io>
2020-04-26 17:06:23 +0200

    fix & add questions

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,8 +1,9 @@
-import codeBlock from "../components/code-block"
+/* eslint-disable */
+import CodeBlock from "../components/code-block"
 
 const Components = {
   pre: (props) => props.children,
-  code: (props) => codeBlock(props),
+  code: (props) => CodeBlock(props),
 }
 
 export default Components
