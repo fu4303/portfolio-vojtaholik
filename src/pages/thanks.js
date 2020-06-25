dd3ef8fa8fecf1689d42a93d448de51860e14bec
Vojta Holik <vojta@egghead.io>
2020-06-25 20:42:04 +0200

    improve /thanks by using url params from calendly

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,18 +1,66 @@
 /** @jsx jsx */
+import React from "react"
 import { graphql } from "gatsby"
 import { jsx, Styled, Image } from "theme-ui"
 import Layout from "../components/layout"
-import HighFive from "../images/high-five.gif"
+import Telephone from "../images/scheduled.gif"
+import dayjs from "dayjs"
+import advancedFormat from "dayjs/plugin/advancedFormat"
+import { isEmpty } from "lodash"
+
+dayjs.extend(advancedFormat)
 
 const ThanksPage = ({
   data: {
     site: { siteMetadata },
   },
+  location,
 }) => {
+  const urlParams = new URLSearchParams(location.search)
+  const date = isEmpty(urlParams.get("event_start_time"))
+    ? null
+    : urlParams.get("event_start_time")
+  const firstName = urlParams.get("invitee_first_name")
+  const event = urlParams.get("event_type_name")
+
+  const [meeting, setMeeting] = React.useState({
+    date: null,
+    firstName: null,
+    event: null,
+  })
+  React.useEffect(() => setMeeting({ date, firstName, event }), [
+    date,
+    firstName,
+    event,
+  ])
   return (
     <Layout {...siteMetadata}>
-      <Styled.h1 sx={{ mt: "6px" }}>Thanks, see ya there!</Styled.h1>
-      <Image src={HighFive} alt="Doggo giving you high five!" />
+      {meeting.date ? (
+        <>
+          <Styled.h1 sx={{ mt: "6px" }}>Booyah, {meeting.firstName}!</Styled.h1>
+          <Styled.p>
+            Our {decodeURI(meeting.event)} is scheduled for{" "}
+            <Styled.strong>
+              {dayjs(meeting.date).format("dddd, MMMM Do")} {" at "}
+              {dayjs(meeting.date).format("h:mm A")}
+            </Styled.strong>
+            . The meeting is via{" "}
+            <Styled.a href="https://zoom.us/">Zoom</Styled.a>, and you should
+            receive all the details on your email at any time now. I&apos;m
+            looking forward to it. See you there!
+          </Styled.p>
+        </>
+      ) : (
+        <>
+          <Styled.h1 sx={{ mt: "6px" }}>All scheduled, thanks!</Styled.h1>
+          <Styled.p>
+            The meeting is via <Styled.a href="https://zoom.us/">Zoom</Styled.a>
+            , and you should receive all the details on your email at any time
+            now. I&apos;m looking forward to it. See you there!
+          </Styled.p>
+        </>
+      )}
+      <Image src={Telephone} alt="Dial phone gif" />
     </Layout>
   )
 }
